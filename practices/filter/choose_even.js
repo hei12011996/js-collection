021cb5cb730d0732785673513bf212c0e8cd6586
Antony Chan <antony.chan@oocl.com>
2018-11-21 14:21:04 +0800

    implement choose_even() using loop

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,13 @@
 function choose_even(collection) {
 
   //implement here
+  let returnArray = [];
+  collection.forEach(function(element){
+  	if(element % 2 == 0){
+  		returnArray.push(element);
+  	}
+  });
+  return returnArray;
 }
 
 module.exports = choose_even;
