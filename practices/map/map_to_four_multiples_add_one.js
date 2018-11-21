9d066e7c21181be8a5e8c98e5ec888647a564ffc
Antony Chan <antony.chan@oocl.com>
2018-11-21 14:22:08 +0800

    implement map_to_four_multiples_add_one() using loop way

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,11 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+  // return [5,9,13,17,21];
+  let returnArray = [];
+  collection.forEach(function(element){
+  	returnArray.push(element * 4 + 1);
+  });
+  return returnArray;
 };
 
 module.exports = map_to_four_multiples_add_one;
