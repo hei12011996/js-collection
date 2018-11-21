47f30ef520c207ab49d256cdabb24f7b88edb542
Antony Chan <antony.chan@oocl.com>
2018-11-21 14:27:27 +0800

    implement grouping_count() using filter

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -4,12 +4,12 @@ function grouping_count(collection) {
 
   //implement here
   let returnCountMap = {};
-  collection.forEach(function(element){
-  	if(returnCountMap[element] === undefined){
-  		returnCountMap[element] = 1;
-  	} else {
-  		returnCountMap[element]++;
-  	}
+  collection.filter(element => {
+    if(returnCountMap[element] === undefined){
+      returnCountMap[element] = 1;
+    } else {
+      returnCountMap[element]++;
+    }
   });
   return returnCountMap;
 }
