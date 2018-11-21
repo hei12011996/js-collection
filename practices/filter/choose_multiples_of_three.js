f177f3b5ab420a1b04bfa361dedd28e03c8d632c
Antony Chan <antony.chan@oocl.com>
2018-11-21 14:21:26 +0800

    implement choose_multiples_of_three() using loop way

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,13 @@
 function choose_multiples_of_three(collection) {
 
   //implement here
+  let returnArray = [];
+  collection.forEach(function(element){
+  	if(element % 3 == 0){
+  		returnArray.push(element);
+  	}
+  });
+  return returnArray;
 }
 
 module.exports = choose_multiples_of_three;
