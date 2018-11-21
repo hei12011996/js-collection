89560aae2cb403e36575762acfad66d52986c725
Antony Chan <antony.chan@oocl.com>
2018-11-21 14:21:37 +0800

    implement grouping_count() using loop way

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,15 @@
 function grouping_count(collection) {
 
   //implement here
+  let returnCountMap = {};
+  collection.forEach(function(element){
+  	if(returnCountMap[element] === undefined){
+  		returnCountMap[element] = 1;
+  	} else {
+  		returnCountMap[element]++;
+  	}
+  });
+  return returnCountMap;
 }
 
 module.exports = grouping_count;
